2c9c07d37762746467fa7b48b7cea53dcfffb146
Giorgio Pomettini <giorgio.freespire@gmail.com>
2019-07-16 02:31:01 +0200

    Finished

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,76 @@
 extern crate clap;
 extern crate reqwest;
 
-use clap::*;
-use reqwest::*;
+use clap::{App, Arg};
 use serde::*;
+use std::collections::HashSet;
 
 const MAX_HASHTAGS: usize = 30;
 
-#[derive(Deserialize, Debug)]
-struct Words {
+#[derive(Deserialize)]
+struct JsonResult {
     word: String,
     score: usize,
+}
+
+#[derive(Default)]
+struct Context {
+    all_tags: Vec<Vec<String>>,
+    output_tags: HashSet<String>,
+}
+
+impl Context {
+    fn process_hashtags(&mut self) {
+        let mut counter = 0;
+        loop {
+            for tags in &mut self.all_tags {
+                match tags.pop() {
+                    Some(tag) => {
+                        self.output_tags.insert(tag);
+                    }
+                    None => continue,
+                }
+            }
+
+            counter += 1;
+
+            if counter > MAX_HASHTAGS {
+                break;
+            }
+        }
+    }
+
+    fn print_hashtags(&self) -> String {
+        self.output_tags
+            .clone()
+            .into_iter()
+            .map(|hashtag| format!("#{} ", hashtag))
+            .collect::<String>()
+    }
 }
 
 fn main() {
     let matches = App::new("Instagram Hashtag Generator")
         .version("0.1.0")
         .author("Giorgio Pomettini <giorgio.pomettini@gmail.com>")
-        .arg(Arg::with_name("hashtags").multiple(true))
+        .arg(Arg::with_name("hashtags").required(true).multiple(true))
         .get_matches();
+
+    let mut ctx: Context = Default::default();
 
     let input_hashtags: Vec<&str> = matches.values_of("hashtags").unwrap().collect();
 
-    let mut current_hashtags: Vec<&str> = input_hashtags;
+    for hashtag in &input_hashtags {
+        let url = format!("https://api.datamuse.com/words?rel_trg={}", hashtag);
+        let json_result: Vec<JsonResult> = reqwest::get(&url).unwrap().json().unwrap();
+        let words: Vec<String> = json_result
+            .iter()
+            .map(|result| result.word.clone())
+            .collect();
+        ctx.all_tags.push(words);
+    }
 
-    let words: Vec<Words> = reqwest::get("https://api.datamuse.com/words?rel_trg=cow")
-        .unwrap()
-        .json()
-        .unwrap();
+    ctx.process_hashtags();
 
-    println!("{:?}", words);
-
-    // while current_hashtags.len() < MAX_HASHTAGS {
-
-    // }
+    println!("{}", &ctx.print_hashtags());
 }
